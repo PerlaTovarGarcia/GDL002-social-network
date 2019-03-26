6b07d736c22daa8e5591730efdf90e94aac062c1
ItzelMB <it.mobec@gmail.com>
2019-03-26 13:22:44 -0600

    Nuevas claves de acceso a firebase y estructura boilerplate

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyANXB1QICy5UrOliUFpOzNOBt_2fKyC0_M",
+    authDomain: "poua-8a563.firebaseapp.com",
+    databaseURL: "https://poua-8a563.firebaseio.com",
+    projectId: "poua-8a563",
+    storageBucket: "",
+    messagingSenderId: "777091228447"
+};
+firebase.initializeApp(config);
+
