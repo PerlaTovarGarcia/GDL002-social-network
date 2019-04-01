d6baa474484f5c2879592189c2484cea66c6ca6f
ItzelMB <it.mobec@gmail.com>
2019-04-01 14:37:13 -0600

    Implementación de bootstraps en nav

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("btnmenu").addEventListener('click', () => {
+    document.getElementById("btnmenu").style.width = '-5rem';
+});
+document.getElementById("menuCloseBtn").addEventListener('click', () => {
+    document.getElementById("menuCloseBtn").style.width = '0rem';
+});
